6e7eed0129ce7c974fc9b3e675610fc38cfdaa43
wevertonsantos <wevercanal@gmail.com>
2026-07-16 18:56:26 -0300

    tentando resolver problema

--- python/romantointeger.py
+++ python/romantointeger.py
@@ -1,32 +1,39 @@
-s = "III"
+s = "MDLXX"
 def romanToInt(s: str) -> int:
     sum = 0
     for i in range(len(s)):
         if s[i] == "I":
-            if s[i + 1] == "V":
-                sum += 4
-            elif s[i + 1] == "X":
-                sum += 9
-            else:
+            if not ((len(s)) <= i + 1):
+                if s[i + 1] == "V":
+                    sum += 4
+                elif s[i + 1] == "X":
+                    sum += 9
+                else:
+                    sum += 1
+            elif s[i] == s[-1]:
                 sum += 1
         elif s[i] == "V":
             if not s[i - 1] == "I":
                 sum += 5
         elif s[i] == "X":
-            if s[i + 1] == "L":
-                sum += 40
-            elif s[i + 1] == "C":
-                sum += 90
-            else:
-                sum += 10
+            if not ((len(s)) <= i + 1):
+                if s[i + 1] == "L":
+                    sum += 40
+                elif s[i + 1] == "C":
+                    sum += 90
+                else:
+                    sum += 10
         elif s[i] == "L":
             if not s[i - 1] == "X":
                 sum += 50
         elif s[i] == "C":
-            if s[i + 1] == "D":
-                sum += 400
-            elif s[i + 1] == "M":
-                sum += 900
+            if not ((len(s)) <= i + 1):
+                if s[i + 1] == "D":
+                    sum += 400
+                elif s[i + 1] == "M":
+                    sum += 900
+                elif not s[i - 1] == "X":
+                    sum += 100
             elif not s[i - 1] == "X":
                 sum += 100
         elif s[i] == "D":

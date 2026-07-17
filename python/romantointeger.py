7fecc2f6e7fee2948174da3fc5130dda284b4685
wevertonsantos <wevercanal@gmail.com>
2026-07-17 17:02:34 -0300

    começando problema dnv

--- python/romantointeger.py
+++ python/romantointeger.py
@@ -1,47 +1,6 @@
 s = "MDLXX"
 def romanToInt(s: str) -> int:
-    sum = 0
-    for i in range(len(s)):
-        if s[i] == "I":
-            if not ((len(s)) <= i + 1):
-                if s[i + 1] == "V":
-                    sum += 4
-                elif s[i + 1] == "X":
-                    sum += 9
-                else:
-                    sum += 1
-            elif s[i] == s[-1]:
-                sum += 1
-        elif s[i] == "V":
-            if not s[i - 1] == "I":
-                sum += 5
-        elif s[i] == "X":
-            if not ((len(s)) <= i + 1):
-                if s[i + 1] == "L":
-                    sum += 40
-                elif s[i + 1] == "C":
-                    sum += 90
-                else:
-                    sum += 10
-        elif s[i] == "L":
-            if not s[i - 1] == "X":
-                sum += 50
-        elif s[i] == "C":
-            if not ((len(s)) <= i + 1):
-                if s[i + 1] == "D":
-                    sum += 400
-                elif s[i + 1] == "M":
-                    sum += 900
-                elif not s[i - 1] == "X":
-                    sum += 100
-            elif not s[i - 1] == "X":
-                sum += 100
-        elif s[i] == "D":
-            if not s[i - 1] == "C":
-                sum += 500
-        elif s[i] == "M":
-            if not s[i - 1] == "C":
-                sum += 1000
-    return sum
+    ...
+    
 
-print(romanToInt(s))
+#print(romanToInt(s))
